824a173082d422282aff2a41a8df59baa69370bd
savejobar <normchel1990@gmail.com>
2024-10-19 11:54:23 +0300

    test1

--- 8mar.c
+++ 8mar.c
@@ -3,7 +3,7 @@
 
 int main ()
 {
-    double x1 = 0, x2 = 2, xacc = 1e-8;
+    double x1 = 0, x2 = 2, xacc = 1e-7;
     int err = 0;
     printf ("Method 1:\n");
     printf ("x = %lf\n\n",rtbis(func,x1,x2,xacc,&err));
