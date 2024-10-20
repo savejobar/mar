6b36ec881a99cc6ae9d144fd9d0be92953e91209
savejobar <normchel1990@gmail.com>
2024-10-20 22:00:50 +0300

    Almost Best

--- 4mar.c
+++ 4mar.c
@@ -1,7 +1,5 @@
 #include <stdio.h> 
 #include <math.h>
-#include <stdlib.h>
-#include <time.h>
 
 typedef double (*finteg) (double);
 double trapz (finteg, double, double, int);
@@ -13,20 +11,19 @@ int n = 1000;
 
 int main() 
 {   
-    clock_t start = clock();
-    double a = 3, b = 10.0, eps = 1e-4;
+    double a = 3, b = 10.0, eps = 1e-6;
+
     printf ("trapz: %lf\n",trapz (function,a,b,n));
     printf ("simpson: %lf\n",simpson (function,a,b,n));
     printf ("trapz_adapt: %lf\n",trapz_adapt(function,a,b,eps));
-    clock_t end = clock();
-    double seconds = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("\nThe time of work: %f seconds\n", seconds);
 }
 
 double trapz (finteg f, double a, double b, int n)
-{   double h = (b-a) / n;
-    double x0 = a, x1 = a + h, xn = x1 + n*h;
-    double sum=0.5*f(xn) +  0.5*f(x0);
+{   
+    double h = (b-a) / n;
+    double x1 = a + h;
+    double sum=0.5*f(x1 + n*h) +  0.5*f(a);
+
     for (int i = 1; i <= n-1; i++)
     {
         sum += f(x1);
@@ -40,6 +37,7 @@ double simpson (finteg f, double a, double b, int n)
     double h = (b-a) / n;
     double x0 = a, x1 = a + h, x2 = x1+h, xn = x1 + n*h, xn_minus_1 = xn - h, xn_minus_2 = xn -2*h , x3 = x2 + h;
     double sum = 3*(f(x0) + f(xn))/8 + 7*(f(x1) + f(xn_minus_1))/6 + 23*(f(x2) + f(xn_minus_2))/24;
+
     for (int i = 3; i <= n-3; i++)
     {
         sum += f(x3);
@@ -50,8 +48,9 @@ double simpson (finteg f, double a, double b, int n)
 
 double trapz_adapt (finteg f, double a, double b, double eps)
 {
-    int n = 1000, precision = 2;
+    int n = 1000, precision = 2.0;
     double  x_new, q0 = trapz (function,a,b,n), q1 = q0, h = (b-a) / n;
+
     do
     {
         h /= precision;
